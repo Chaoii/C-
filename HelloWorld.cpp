5345ee5d1e5f8e8b7ca9bb8e9eeab53bf647d484
Chaoii <xuchao00159@gmail.com>
2016-12-18 10:33:05 +0800

    add distributed

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: HelloWorld.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 日 12/18 10:17:39 2016
+ ************************************************************************/
+//编写程序在标准输出上打印HelloWorld
+#include<iostream>
+using namespace std;
+int main()
+{
+    std::cout<<"HelloWorld"<<endl;
+    return 0;
+}
